079ba54ca04c35b1ca59e999393ef66e9b36dc02
Philip Potter <philipgpotter@gmail.com>
2012-06-26 19:31:38 +0100

    initial commit

---
+++ src/chatbot/core.clj
@@ -0,0 +1,1 @@
+(ns chatbot.core)
---
+++ test/chatbot/test/core.clj
@@ -0,0 +1,6 @@
+(ns chatbot.test.core
+  (:use [chatbot.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
