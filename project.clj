4979cf63be1ae94d678f4eccd49907ecd0cddc79
Philip Potter <philipgpotter@gmail.com>
2012-06-27 09:22:13 +0100

    rudimentary chat bot
    Authored by: Valentin, Kush, Ian and Phil
    
    This bot loops over stdin, reading a line, splitting into words,
    removing common words, searching twitter using these words as
    criteria, ignoring messages which are @someone, and finally
    stripping hashtags, http: urls, and things which look like html
    entities.

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject chatbot "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :main        chatbot.core
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [jline "0.9.94"]
+                 [org.clojure/data.json "0.1.2"]
+                 ])
--- src/chatbot/core.clj
+++ src/chatbot/core.clj
@@ -1,1 +1,44 @@
-(ns chatbot.core)
+(ns chatbot.core
+  (:use [clojure.data.json :only (read-json)]
+        [clojure.pprint])
+  (:require [clojure.string])
+  (:gen-class))
+
+(def common-words
+  #{"is" "a" "the" "are" "an" "and" "what" "why" "he" "she" "it" "i" "in" "me" "you" "they" "him" "can" "should" "will" "have" "for" "I'm" "I"})
+
+(defn entity? [word]
+  (.startsWith word "&"))
+
+(defn url? [poss-url]
+  (.startsWith poss-url "http:"))
+
+(defn hashtag? [word]
+  (.startsWith word "#"))
+
+(defn without [pred tweet]
+  (let [words (seq (.split tweet " "))]
+    (clojure.string/join " " (remove pred words))))
+
+(defn twitter-search [terms]
+  (let [concat-terms (clojure.string/join
+                      "+" (remove common-words terms))
+        uri    (str "http://search.twitter.com/search.json?q="
+                 concat-terms)
+        result (read-json (slurp (java.net.URI. uri)))
+        tweet  (or (first (filter
+                           (complement (partial some #{\@}))
+                           (map :text (:results result))))
+                   "sorry, I don't understand")]
+    ; originally:
+    ; (without entity? (without hashtag? (without url? tweet)))
+    (reduce #(without %2 %1) tweet [entity? hashtag? url?])))
+
+(defn -main [& args]
+  (loop []
+    (print "> ")
+    (flush)
+    (println (twitter-search (.split (read-line) "\\s+")))
+    (recur)))
+
+
